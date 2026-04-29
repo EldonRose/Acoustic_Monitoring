92f6a027b9476ac5b1302a2db454e5644fd4e6dc
EldonRose <rose@eldonfarms.com>
2026-04-29 16:12:48 -0400

    Update Subset_by_threshold.R

--- Acoustic_Monitoring/Subset_by_threshold.R
+++ Acoustic_Monitoring/Subset_by_threshold.R
@@ -1,10 +1,26 @@
-# Set wd
-setwd("C:/DesktopFolder/Eldon")
+# Subset by Prediction Probability
 
-# Rose's Notes
-  # 1. copy/paste all CSVs into WD
-  # 2. https://stackoverflow.com/questions/11433432/how-to-import-multiple-csv-files-at-once
+setwd("C:/Users/rwetz/Documents/GitHub/Acoustic_Monitoring/Acoustic_Monitoring")
 
-# import all selection tables:
-blugrb1 <- read.table("C:/DesktopFolder/Eldon/Blue_Grosbeak_Final.txt",
-                      sep = "\t", header = TRUE)
+# Confidence Thresholds by Probability (p=0.9) for focal spp:
+  # Blue Grosbeak (blugrb1) = 0.617
+  # Common Yellowthroat (comyel) = 0.177
+  # Eastern Meadowlark (easme) = 0.249
+  # Field Sparrow (fiespa) = 0.714
+  # Upland Sandpiper (uplsan) = 0.5
+  # Horned Lark (horlar) = 0.858
+  # Song Sparrow (sonspa) = 0.524
+
+# TO DO # 
+  # 1. MANUALLY add date column to all CSVs
+  # 2. Import CSVs as data frames together by RecorderNumber
+    # https://www.statology.org/r-merge-csv-files/
+  # 3. Add Recorder# as column to each data frame
+  # 4. Add sample ID to each call row in data frame
+    # SAVE DATA FRAME AS BACKUP CSV
+  # 5. Subset to include only data above each spp's threshold
+  # 6. Add subset data frames to master dataframe
+
+# To do later #
+  # 7. Add treatments
+
