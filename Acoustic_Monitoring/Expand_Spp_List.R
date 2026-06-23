cd5a7e5867a19b1532aabb5f38bfc7186c0d0e29
EldonRose <rose@eldonfarms.com>
2026-06-23 10:20:11 -0400

    Species list generation

--- Acoustic_Monitoring/Expand_Spp_List.R
+++ Acoustic_Monitoring/Expand_Spp_List.R
@@ -17,30 +17,31 @@ meta_model <- birdnet_model_meta("v2.4")
 
 # Predict species no weeks
 list <- predict_species_at_location_and_time(
-  meta,
+  meta_model,
   38.596400,
   -78.186269,
   week = NULL,
   min_confidence = 0.02
 )
 
-# Predict species within project weeks
-list2 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 15, min_confidence = 0.02)
-list3 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 16, min_confidence = 0.02)
-list4 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 17, min_confidence = 0.02)
-list5 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 18, min_confidence = 0.02)
-list6 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 19, min_confidence = 0.02)
-list7 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 20, min_confidence = 0.02)
-list8 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 21, min_confidence = 0.02)
-list9 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 22, min_confidence = 0.02)
-list10 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 23, min_confidence = 0.02)
-list11 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 24, min_confidence = 0.02)
-list12 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 25, min_confidence = 0.02)
-list13 <- predict_species_at_location_and_time(meta, 38.596400, -78.186269, week = 26, min_confidence = 0.02)
+# Predict species within project weeks (2025)
+list2 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 15, min_confidence = 0.02)
+list3 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 16, min_confidence = 0.02)
+list4 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 17, min_confidence = 0.02)
+list5 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 18, min_confidence = 0.02)
+list6 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 19, min_confidence = 0.02)
+list7 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 20, min_confidence = 0.02)
+list8 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 21, min_confidence = 0.02)
+list9 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 22, min_confidence = 0.02)
+list10 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 23, min_confidence = 0.02)
+list11 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 24, min_confidence = 0.02)
+list12 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 25, min_confidence = 0.02)
+list13 <- predict_species_at_location_and_time(meta_model, 38.596400, -78.186269, week = 26, min_confidence = 0.02)
 
 weeks <- bind_rows(list2, list3, list4, list5, list6, list7, list8, list9, list10, list11, list12, list13)
 names <- as.data.frame(weeks$label)
 all.sp <- as.data.frame(unique(names))
+write.csv(all.sp, "C:/Users/rwetz/Documents/GitHub/Acoustic_Monitoring/Acoustic_Monitoring/Expanded_Sp_2025.csv")
 
 # To do:
   # Refine all.sp list to exclude irrelevant birds (e.g. forest, shorebirds)
