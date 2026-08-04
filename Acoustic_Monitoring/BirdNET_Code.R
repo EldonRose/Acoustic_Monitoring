8a25dbbda00076da6a1d4033a5d62cbc3bc8f407
EldonRose <rose@eldonfarms.com>
2026-08-04 13:30:42 -0400

    Models and individual audio paths

--- Acoustic_Monitoring/BirdNET_Code.R
+++ Acoustic_Monitoring/BirdNET_Code.R
@@ -4,9 +4,15 @@
 
 library(birdnetR)
 
-# Initialize the TensorFlow Lite model
-model <- birdnet_model_tflite("v2.4")
+# Initialize the TensorFlow Lite model - DO NOT RUN
+# model <- birdnet_model_tflite("v2.4")
+
+# Initialize Protobuf Model - DO RUN
+model <- birdnet_model_protobuf("v2.4")
 
 # Path to an example audio file (replace with your own file path)
-audio_path <- "C:/Users/rwetz/Documents/GitHub/Acoustic_Monitoring/Acoustic_Monitoring/TEST2.wav"
+audio_path_rw <- "C:/Users/rwetz/Documents/GitHub/Acoustic_Monitoring/Acoustic_Monitoring/TEST2.wav"
+audio_path_cs <- "Christian replace this with your path to audio file"
+
 predictions <- predict_species_from_audio_file(model, audio_path, min_confidence = 0.3, keep_empty = FALSE)
+  # ROSE: need to add sp list once finished
